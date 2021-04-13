086ba5575493f3e78876ed27d70e4f6a6f612bc1
aderemi-adesada <adesadaaderemi@gmail.com>
2021-04-13 16:28:17 +0100

    v 0.2.3

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "Nagato",
     "author": "Adesada J. Aderemi, Taiwo Folu",
-    "version": (0, 2, 2),
+    "version": (0, 2, 3),
     "blender": (2, 80, 0),
     "location": "View3D > Add > Mesh > New Object",
     "description": "Perform version control commands and project managements",
