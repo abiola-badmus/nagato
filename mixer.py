3b2ee5a51c033e6ea9e2d676c1afa427a77620bf
aderemi-adesada <adesadaaderemi@gmail.com>
2021-06-01 17:29:20 +0100

    used f string

--- mixer.py
+++ mixer.py
@@ -19,7 +19,7 @@ import shutil
 
 
 def process_exists(process_name):
-    call = 'TASKLIST', '/FI', 'imagename eq %s' % process_name
+    call = 'TASKLIST', '/FI', f'imagename eq {process_name}'
     # use buildin check_output right away
     output = subprocess.check_output(call).decode()
     # check in last line for process name
